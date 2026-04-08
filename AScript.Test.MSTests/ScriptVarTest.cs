a065e8d164575d30ab7cf4e67d8a2739b91a2b7b
rockey627 <584532801@qq.com>
2026-04-08 22:00:27 +0800

    1.0.1.1

--- AScript.Test.MSTests/ScriptVarTest.cs
+++ AScript.Test.MSTests/ScriptVarTest.cs
@@ -10,6 +10,41 @@ namespace AScript.Test.MSTests
 	public class ScriptVarTest
 	{
 		[TestMethod]
+		public void Test03_2()
+		{
+			string s = "5+n";
+			var script = new AScript.Script();
+			script.Options.CompileMode = ECompileMode.All;
+			try
+			{
+				script.Eval(s);
+			}
+			catch (Exception ex)
+			{
+				Assert.AreEqual("variable n is not exists", ex.Message);
+				return;
+			}
+			Assert.IsTrue(false);
+		}
+
+		[TestMethod]
+		public void Test03()
+		{
+			string s = "5+n";
+			var script = new AScript.Script();
+			try
+			{
+				script.Eval(s);
+			}
+			catch(Exception ex)
+			{
+				Assert.AreEqual("variable n is not exists", ex.Message);
+				return;
+			}
+			Assert.IsTrue(false);
+		}
+
+		[TestMethod]
 		public void Test02()
 		{
 			string s = "int v=5;100 * (v + 5) * (6-2)";
