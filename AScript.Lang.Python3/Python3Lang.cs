ac1f562494647eb777974202928c911f82b6740a
rockey627 <584532801@qq.com>
2026-04-17 23:01:45 +0800

    AScript.Lang.Python3

---
+++ AScript.Lang.Python3/Python3Lang.cs
@@ -0,0 +1,79 @@
+﻿using AScript.Lang.Python3.Readers;
+using AScript.Lang.Python3.TokenHandlers;
+using AScript.Nodes;
+using AScript.Operators;
+using AScript.Readers;
+using AScript.Syntaxs;
+using AScript.TokenHandlers;
+using System;
+using System.Collections.Generic;
+
+namespace AScript.Lang.Python3
+{
+	public class Python3Lang : ScriptLang
+	{
+		public static readonly Python3Lang Instance = new Python3Lang();
+
+		public static readonly HashSet<string> EndTokens = new HashSet<string>() { ":", "\n" };
+
+		public Python3Lang()
+		{
+			AddFunc("=", AssignOperator.Instance);
+			AddFunc("+", PlusOperator.Instance);
+			AddFunc("-", SubtractOperator.Instance);
+			AddFunc("*", MultiplyOperator.Instance);
+			AddFunc("/", DivideOperator.Instance);
+			AddFunc("<", LessThanOperator.Instance);
+			AddFunc(">", GreaterThanOperator.Instance);
+			AddFunc(">=", GreaterThanOrEqualOperator.Instance);
+			AddFunc("<=", LessThanOrEqualOperator.Instance);
+			AddFunc("==", EqualOperator.Instance);
+			AddFunc("!=", NotEqualOperator.Instance);
+			AddFunc("and", AndAlsoOperator.Instance);
+			AddFunc("or", OrElseOperator.Instance);
+
+			AddTokenHandler("and", AndAlsoTokenHandler.Instance);
+			AddTokenHandler("or", OrElseTokenHandler.Instance);
+			AddTokenHandler("if", TokenHandlers.IfTokenHandler.Instance);
+			AddTokenHandler("def", TokenHandlers.DefTokenHandler.Instance);
+			AddTokenHandler("return", ReturnTokenHandler.Instance);
+			// python中不能使用#lang，用@lang代替
+			AddTokenHandler("@lang", new LangTokenHandler("@end"));
+		}
+
+		public override ITokenStream GetTokenStream(CharReader charReader)
+		{
+			return new Python3TokenStream(charReader);
+		}
+
+		public static TreeBuilder BuildSubBlock(int parentColumn, DefaultSyntaxAnalyzer analyzer, BuildContext buildContext, ScriptContext scriptContext, BuildOptions options, TokenReader tokenReader, EvalControl control, bool ignore = false, IEnumerable<string> endTokens = null)
+		{
+			var token = tokenReader.Read();
+			if (!token.HasValue) return null;
+			if (token.Value.Column <= parentColumn)
+			{
+				tokenReader.Push(token.Value);
+				return null;
+			}
+
+			var builder = ignore ? null : new TreeBuilder();
+			int column = token.Value.Column;
+			while (token.HasValue && token.Value.Column == column)
+			{
+				tokenReader.Push(token.Value);
+				var statement = analyzer.BuildOneStatement(buildContext, scriptContext, options, tokenReader, control, ignore, endTokens: endTokens);
+				if (!ignore)
+				{
+					builder.Add(buildContext, scriptContext, options, control, statement);
+				}
+				token = tokenReader.Read();
+			}
+			if (token.HasValue)
+			{
+				tokenReader.Push(token.Value);
+			}
+
+			return builder;
+		}
+	}
+}
---
+++ AScript.Lang.Python3/TokenHandlers/IfTokenHandler.cs
@@ -0,0 +1,62 @@
+﻿using AScript.Nodes;
+using AScript.Syntaxs;
+using System;
+
+namespace AScript.Lang.Python3.TokenHandlers
+{
+	/// <summary>
+	/// <para>if expression :</para>
+	/// <para>    statement1</para>
+	/// <para>    statement2</para>
+	/// <para>elif expression :</para>
+	/// <para>    statement1</para>
+	/// <para>    statement2</para>
+	/// <para>else :</para>
+	/// <para>    statement1</para>
+	/// <para>    statement2</para>
+	/// <para>other</para>
+	/// </summary>
+	public class IfTokenHandler : ITokenHandler
+	{
+		public static readonly IfTokenHandler Instance = new IfTokenHandler();
+
+		public void Build(DefaultSyntaxAnalyzer analyzer, TokenAnalyzingArgs e)
+		{
+			e.IsHandled = true;
+			e.End = true;
+
+			// if语句中不能立即执行，所以要先创建完整的if表达式树再编译或执行
+			var createFullOptions = new BuildOptions(e.Options) { CreateFullTreeNode = true };
+			var condition = analyzer.BuildOneStatement(e.BuildContext, e.ScriptContext, createFullOptions, e.TokenReader, e.Control, e.Ignore, endTokens: Python3Lang.EndTokens);
+			analyzer.ValidateNextToken(e.TokenReader, ":");
+			ITreeNode bodyNode = Python3Lang.BuildSubBlock(e.CurrentToken.Column, analyzer, e.BuildContext, e.ScriptContext, createFullOptions, e.TokenReader, e.Control, e.Ignore, endTokens: Python3Lang.EndTokens);
+			//
+			ITreeNode elseNode = null;
+			var token = e.TokenReader.Read();
+			if (token.HasValue && token.Value.Column == e.CurrentToken.Column)
+			{
+				if (token.Value.Type != ETokenType.String && token.Value.Value == "elif")
+				{
+					// else if
+					e.TokenReader.Push(new Token("if", ETokenType.Word, token.Value.Line, token.Value.Column));
+					elseNode = analyzer.BuildOneStatement(e.BuildContext, e.ScriptContext, createFullOptions, e.TokenReader, e.Control, e.Ignore, endTokens: Python3Lang.EndTokens);
+				}
+				else if (token.Value.Type != ETokenType.String && token.Value.Value == "else")
+				{
+					analyzer.ValidateNextToken(e.TokenReader, ":");
+					elseNode = Python3Lang.BuildSubBlock(e.CurrentToken.Column, analyzer, e.BuildContext, e.ScriptContext, createFullOptions, e.TokenReader, e.Control, e.Ignore, endTokens: Python3Lang.EndTokens);
+				}
+				else
+				{
+					e.TokenReader.Push(token.Value);
+				}
+			}
+			else
+			{
+				e.TokenReader.Push(token.Value);
+			}
+
+			e.TreeBuilder.Add(e.BuildContext, e.ScriptContext, e.Options, e.Control, new IfNode { Condition = condition, Body = bodyNode, Else = elseNode });
+		}
+	}
+}
