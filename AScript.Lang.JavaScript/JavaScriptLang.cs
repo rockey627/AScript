d9d98100dfdc00ad995a2c5979bed5104e4c9fdc
rockey627 <584532801@qq.com>
2026-04-17 23:02:26 +0800

    AScript.Lang.JavaScript

---
+++ AScript.Lang.JavaScript/JavaScriptLang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AScript.Lang.JavaScript
+{
+	/// <summary>
+	/// js脚本语言
+	/// </summary>
+	public class JavaScriptLang : ScriptLang
+	{
+		public JavaScriptLang()
+		{
+		}
+	}
+}
