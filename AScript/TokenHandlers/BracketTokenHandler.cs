3271ce20733a066173e8fc9b1b49a0090f033be4
rockey627 <584532801@qq.com>
2026-04-21 10:28:58 +0800

    BracketTokenHandler创建数组

--- AScript/TokenHandlers/BracketTokenHandler.cs
+++ AScript/TokenHandlers/BracketTokenHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using AScript.Nodes;
 using AScript.Syntaxs;
 
@@ -51,9 +52,73 @@ namespace AScript.TokenHandlers
 
 		protected virtual bool BuildCollection(DefaultSyntaxAnalyzer analyzer, TokenAnalyzingArgs e)
 		{
-			// 创建数组：[1,2,3,4]
+			// 解析 [1,2,3,4] 数组
+			var args = e.Ignore ? null : new List<ITreeNode>();
+			var tokenReader = e.TokenReader;
+
+			while (true)
+			{
+				var element = analyzer.BuildOneStatement(e.BuildContext, e.ScriptContext, e.Options, tokenReader, e.Control, e.Ignore);
+				if (!e.Ignore)
+				{
+					args.Add(element);
+				}
+
+				var nextToken = tokenReader.Read();
+				if (!nextToken.HasValue)
+				{
+					throw new Exception($"invalid expression at {tokenReader.CharReader.CurrentLine},{tokenReader.CharReader.CurrentColumn}, expect ']'");
+				}
+				if (nextToken.Value.Type == ETokenType.String)
+				{
+					throw new Exception($"invalid expression at {nextToken.Value.Line},{nextToken.Value.Column}, expect ',' or ']'");
+				}
+				if (nextToken.Value.Value == "]")
+				{
+					break;
+				}
+
+				if (nextToken.Value.Value != ",")
+				{
+					throw new Exception($"invalid expression at {nextToken.Value.Line},{nextToken.Value.Column}, expect ',' or ']'");
+				}
+			}
+
+			if (!e.Ignore)
+			{
+				var arr = CreateCollection(args.Select(a => a.Eval(e.ScriptContext, e.Options, e.Control, out _)).ToList());
+				var arrNode = PoolManage.CreateObjectData(arr);
+				e.TreeBuilder.AddData(e.BuildContext, e.ScriptContext, e.Options, e.Control, arrNode);
+			}
 
 			return true;
+		}
+
+		protected virtual object CreateCollection(List<object> list)
+		{
+			Type elementType = null;
+			foreach (var item in list)
+			{
+				if (item == null) continue;
+				var itemType = item.GetType();
+				if (elementType == null)
+				{
+					elementType = itemType;
+					continue;
+				}
+				if (itemType != elementType)
+				{
+					elementType = null;
+					break;
+				}
+			}
+			if (elementType == null) elementType = typeof(object);
+			var arr = Array.CreateInstance(elementType, list.Count);
+			for (int i = 0; i < list.Count; i++)
+			{
+				arr.SetValue(list[i], i);
+			}
+			return arr;
 		}
 
 		protected virtual bool TryBuildNext(DefaultSyntaxAnalyzer analyzer, TokenAnalyzingArgs e, ITreeNode statement0, Token nextToken)
