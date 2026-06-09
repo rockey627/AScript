7b391afce03f2e0ff2f5f8dd88a4d3e2fa9337d9
rockey627 <584532801@qq.com>
2026-06-09 20:55:26 +0800

    JavaScriptFunctionTokenHandler

--- AScript.Lang.JavaScript/JavaScriptLang.cs
+++ AScript.Lang.JavaScript/JavaScriptLang.cs
@@ -1,14 +1,88 @@
-﻿using System;
+﻿using AScript.Functions;
+using AScript.Lang.JavaScript.TokenHandlers;
+using AScript.Operators;
+using AScript.TokenHandlers;
+using System;
 
 namespace AScript.Lang.JavaScript
 {
 	/// <summary>
-	/// js脚本语言
+	/// JavaScript脚本语言
 	/// </summary>
 	public class JavaScriptLang : ScriptLang
 	{
-		public JavaScriptLang()
+		public static readonly JavaScriptLang Instance = new JavaScriptLang();
+
+		protected JavaScriptLang()
 		{
+			AddType("var", typeof(object));
+			AddType("let", typeof(object));
+			AddType("const", typeof(object));
+			AddType("Array", typeof(Array));
+
+			AddFunc("=", AssignOperator.Instance);
+			AddFunc("+=", PlusAssignOperator.Instance);
+			AddFunc("-=", SubtractAssignOperator.Instance);
+			AddFunc("*=", MultiplyAssignOperator.Instance);
+			AddFunc("**=", PowerAssignOperator.Instance);
+			AddFunc("/=", DivideAssignOperator.Instance);
+			AddFunc("%=", ModuloAssignOperator.Instance);
+			AddFunc("^=", XOrAssignOperator.Instance);
+			AddFunc("&=", AndAssignOperator.Instance);
+			AddFunc("|=", OrAssignOperator.Instance);
+			AddFunc("?=", QuestionAssignOperator.Instance);
+			AddFunc(">>=", RightShiftAssignOperator.Instance);
+			AddFunc("<<=", LeftShiftAssignOperator.Instance);
+			AddFunc("??", QuestionQuestionOperator.Instance);
+			AddFunc("?", QuestionIIFOperator.Instance);
+			AddFunc("+", PlusOperator.Instance);
+			AddFunc("-", SubtractOperator.Instance);
+			AddFunc("*", MultiplyOperator.Instance);
+			AddFunc("**", PowerOperator.Instance);
+			AddFunc("/", DivideOperator.Instance);
+			AddFunc("%", ModuloOperator.Instance);
+			AddFunc("&", AndOperator.Instance);
+			AddFunc("|", OrOperator.Instance);
+			AddFunc("^", XOrOperator.Instance);
+			AddFunc("~", NotOperator.Instance);
+			AddFunc("<<", LeftShiftOperator.Instance);
+			AddFunc(">>", RightShiftOperator.Instance);
+			AddFunc("++", IncrementAssignOperator.Instance);
+			AddFunc("--", DecrementAssignOperator.Instance);
+			AddFunc("!", BoolNotOperator.Instance);
+			AddFunc("<", LessThanOperator.Instance);
+			AddFunc(">", GreaterThanOperator.Instance);
+			AddFunc(">=", GreaterThanOrEqualOperator.Instance);
+			AddFunc("<=", LessThanOrEqualOperator.Instance);
+			AddFunc("==", EqualOperator.Instance);
+			AddFunc("!=", NotEqualOperator.Instance);
+			AddFunc("&&", AndAlsoOperator.Instance);
+			AddFunc("||", OrElseOperator.Instance);
+			AddFunc(".", DotOperator.Instance);
+			AddFunc("?.", new DotOperator(true));
+			AddFunc("[]", IndexOperator.Instance);
+			AddFunc("[:]", IndexStartEndOperator.Instance);
+
+			// 内置eval函数
+			AddFunc("eval", EvalFunction.Instance);
+
+			AddTokenHandler("??", LazyTokenHandler.Instance);
+			AddTokenHandler("?=", LazyTokenHandler.Instance);
+			AddTokenHandler("?", QuestionIIFTokenHandler.Instance);
+			AddTokenHandler("[", BracketTokenHandler.Instance);
+			AddTokenHandler("null", NullTokenHandler.Instance);
+			AddTokenHandler("true", BoolTokenHandler.Instance);
+			AddTokenHandler("false", BoolTokenHandler.Instance);
+			AddTokenHandler("new", NewTokenHandler.Instance);
+			AddTokenHandler("return", ReturnTokenHandler.Instance);
+			AddTokenHandler("break", BreakTokenHandler.Instance);
+			AddTokenHandler("continue", ContinueTokenHandler.Instance);
+			AddTokenHandler("function", JavaScriptFunctionTokenHandler.Instance);
+		}
+
+		public override bool IsDynamic()
+		{
+			return true;
 		}
 	}
 }
---
+++ AScript.Lang.JavaScript/TokenHandlers/JavaScriptFunctionTokenHandler.cs
@@ -0,0 +1,70 @@
+using AScript.Nodes;
+using AScript.Syntaxs;
+using System;
+using System.Collections.Generic;
+
+namespace AScript.Lang.JavaScript.TokenHandlers
+{
+	/// <summary>
+	/// function 函数名(参数名1, 参数名2) {
+	///
+	/// }
+	/// </summary>
+	public class JavaScriptFunctionTokenHandler : ITokenHandler
+	{
+		public static readonly JavaScriptFunctionTokenHandler Instance = new JavaScriptFunctionTokenHandler();
+
+		public void Build(DefaultSyntaxAnalyzer analyzer, TokenAnalyzingArgs e)
+		{
+			e.IsHandled = true;
+			e.End = true;
+			if (e.TreeBuilder.IsFullStatement())
+			{
+				e.TokenReader.Push(e.CurrentToken);
+				return;
+			}
+			// 函数名
+			var funcNameToken = analyzer.ValidateNextToken(e.TokenReader, ETokenType.Word);
+			string funcName = funcNameToken.Value.Value;
+			// 参数
+			analyzer.ValidateNextToken(e.TokenReader, "(");
+			var nextToken = e.TokenReader.Read();
+			if (!nextToken.HasValue)
+			{
+				throw new Exceptions.ScriptAnalyzingException($"invalid function {funcName} at ({e.CurrentToken.Line},{e.CurrentToken.Column})");
+			}
+			List<DefineVarNode> args = null;
+			if (!nextToken.Value.IsSymbol(")"))
+			{
+				args = e.Ignore ? null : new List<DefineVarNode>();
+				e.TokenReader.Push(nextToken.Value);
+				while (true)
+				{
+					var argToken = analyzer.ValidateNextToken(e.TokenReader, ETokenType.Word);
+					string argName = argToken.Value.Value;
+					if (!e.Ignore)
+					{
+						args.Add(new DefineVarNode(argName) { SystemType = typeof(object) });
+					}
+					nextToken = e.TokenReader.Read();
+					if (!nextToken.HasValue)
+					{
+						throw new Exceptions.ScriptAnalyzingException($"invalid function {funcName} at ({e.CurrentToken.Line},{e.CurrentToken.Column})");
+					}
+					if (nextToken.Value.IsSymbol(",")) continue;
+					if (nextToken.Value.IsSymbol(")")) break;
+					throw new Exceptions.ScriptAnalyzingException($"invalid argument name '{nextToken.Value.Value}' at ({nextToken.Value.Line},{nextToken.Value.Column})");
+				}
+			}
+			// 函数体
+			var createFullOptions = (e.Options.CreateFullTreeNode ?? false) ? e.Options : new BuildOptions(e.Options) { CreateFullTreeNode = true };
+			var body = analyzer.BuildOneStatement2(e.BuildContext, e.ScriptContext, createFullOptions, e.TokenReader, e.Control, e.Ignore, noblock: true);
+			//
+			if (!e.Ignore)
+			{
+				var defineNode = new DefineFuncNode { Name = funcName, Args = args.ToArray(), Body = body };
+				e.TreeBuilder.AddData(e.BuildContext, e.ScriptContext, e.Options, e.Control, defineNode);
+			}
+		}
+	}
+}
