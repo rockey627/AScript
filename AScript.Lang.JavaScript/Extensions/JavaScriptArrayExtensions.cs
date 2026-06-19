37b677ce04d1949479b57e35e765e6319e4d9b06
rockey627 <584532801@qq.com>
2026-06-19 16:36:24 +0800

    JavaScriptArrayExtensions

--- AScript.Lang.JavaScript/Extensions/JavaScriptArrayExtensions.cs
+++ AScript.Lang.JavaScript/Extensions/JavaScriptArrayExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace AScript.Lang.JavaScript.Extensions
 {
@@ -18,6 +20,165 @@ namespace AScript.Lang.JavaScript.Extensions
 		public static List<object> new_Array(params object[] values)
 		{
 			return new List<object>(values);
+		}
+
+		public static LambdaExpression[] join()
+		{
+			Expression<Func<List<object>, string>> join1 = list => string.Join("", list);
+			Expression<Func<List<object>, string, string>> join2 = (list, separator) => string.Join(separator, list);
+			return new LambdaExpression[] { join1, join2 };
+		}
+
+		public static Expression<Func<List<object>, object, long>> indexOf() => (list, obj) => (long)list.IndexOf(obj);
+
+		public static List<object> reverse(List<object> list)
+		{
+			list.Reverse();
+			return list;
+		}
+
+		public static List<object> fill(List<object> list, object v)
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				list[i] = v;
+			}
+			return list;
+		}
+
+		public static List<object> filter(List<object> list, Func<object, bool> func)
+		{
+			return list.Where(func).ToList();
+		}
+
+		public static long findIndex(List<object> list, Func<object, bool> func)
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (func(list[i])) return i;
+			}
+			return -1L;
+		}
+
+		public static List<object> map(List<object> list, Func<object, object> func)
+		{
+			return list.Select(func).ToList();
+		}
+
+		public static object reduce(List<object> list, Func<object, object, object> func)
+		{
+			if (list.Count == 0) return null;
+			if (list.Count == 1) return list[0];
+			object r = list[0];
+			for (int i = 1; i < list.Count; i++)
+			{
+				r = func(r, list[i]);
+			}
+			return r;
+		}
+
+		public static object reduce(List<object> list, Func<object, object, object> func, object init)
+		{
+			if (list.Count == 0) return init;
+			object r = init;
+			for (int i = 0; i < list.Count; i++)
+			{
+				r = func(r, list[i]);
+			}
+			return r;
+		}
+
+		public static void forEach(List<object> list, Action<object> action)
+		{
+			list.ForEach(action);
+		}
+
+		public static object pop(List<object> list)
+		{
+			if (list == null || list.Count == 0) return null;
+			int index = list.Count - 1;
+			object v = list[index];
+			list.RemoveAt(index);
+			return v;
+		}
+
+		public static object shift(List<object> list)
+		{
+			if (list == null || list.Count == 0) return null;
+			object v = list[0];
+			list.RemoveAt(0);
+			return v;
+		}
+
+		public static long push(List<object> list, object v)
+		{
+			list.Add(v);
+			return list.Count;
+		}
+
+		public static long push(List<object> list, params object[] vs)
+		{
+			if (vs == null || vs.Length == 0) return list.Count;
+			if (vs.Length == 1) list.Add(vs[0]);
+			else list.AddRange(vs);
+			return list.Count;
+		}
+
+		public static long unshift(List<object> list, object v)
+		{
+			list.Insert(0, v);
+			return list.Count;
+		}
+
+		public static long unshift(List<object> list, params object[] vs)
+		{
+			if (vs == null || vs.Length == 0) return list.Count;
+			if (vs.Length == 1) list.Insert(0, vs[0]);
+			else list.InsertRange(0, vs);
+			return list.Count;
+		}
+
+		public static List<object> splice(List<object> list, long index)
+		{
+			int count = list.Count - (int)index;
+			return splice(list, index, count);
+		}
+
+		public static List<object> splice(List<object> list, long index, long count)
+		{
+			if (count == 0) return new List<object>();
+			var result = new List<object>((int)count);
+			for (int i = 0; i < count; i++)
+			{
+				result.Add(list[i + (int)index]);
+			}
+			list.RemoveRange((int)index, (int)count);
+			return result;
+		}
+
+		public static List<object> splice(List<object> list, long index, long count, params object[] addingList)
+		{
+			var result = splice(list, index, count);
+			if (addingList != null && addingList.Length > 0)
+			{
+				list.InsertRange((int)index, addingList);
+			}
+			return result;
+		}
+
+		public static bool every(List<object> list, Func<object, bool> predicate)
+		{
+			return list.All(predicate);
+		}
+
+		public static bool some(List<object> list, Func<object, bool> predicate)
+		{
+			return list.Any(predicate);
+		}
+
+		public static object find(List<object> list, Func<object, bool> predicate)
+		{
+			return list.FirstOrDefault(predicate);
 		}
 	}
 }
--- AScript.Lang.JavaScript/JavaScriptLang.cs
+++ AScript.Lang.JavaScript/JavaScriptLang.cs
@@ -5,7 +5,6 @@ using AScript.Operators;
 using AScript.TokenHandlers;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace AScript.Lang.JavaScript
 {
@@ -75,40 +74,12 @@ namespace AScript.Lang.JavaScript
 			AddFunc("concat", ConcatFunction.Instance);
 			AddFunc("includes", new ContainsFunction());
 			AddFunc("get_length", new LengthFunction(typeof(long)));
+			AddFunc("slice", IndexStartEndOperator.Instance);
 
 			AddFunc(typeof(JavaScriptDateExtensions));
 			AddFunc(typeof(JavaScriptMathExtensions));
 			AddFunc(typeof(JavaScriptStringExtensions));
 			AddFunc(typeof(JavaScriptArrayExtensions));
-
-			AddLambda<Func<List<object>, string>>("join", list => string.Join("", list));
-			AddLambda<Func<List<object>, string, string>>("join", (list,separator) => string.Join(separator, list));
-			AddLambda<Func<List<object>, object, long>>("indexOf", (list, obj) => (long)list.IndexOf(obj));
-			AddFunc<List<object>, List<object>>("reverse", list => { list.Reverse(); return list; });
-			AddFunc<List<object>, object, List<object>>("fill", List_fill);
-			AddFunc<List<object>, Func<object, bool>, List<object>>("filter", List_filter);
-			AddFunc<List<object>, Func<object, bool>, long>("findIndex", List_findIndex);
-			AddFunc<List<object>, Func<object, object>, List<object>>("map", List_map);
-			AddFunc<List<object>, Func<object, object, object>, object>("reduce", List_reduce1);
-			AddFunc<List<object>, Func<object, object, object>, object, object>("reduce", List_reduce2);
-			AddAction<List<object>, Action<object>>("forEach", List_forEach);
-			AddFunc<List<object>, object>("pop", List_pop);
-			AddFunc<List<object>, object>("shift", List_shift);
-			AddFunc<List<object>, object, long>("push", List_push);
-			AddFunc<List<object>, object[], long>("push", List_push);
-			AddFunc<List<object>, object, long>("unshift", List_unshift);
-			AddFunc<List<object>, object[], long>("unshift", List_unshift);
-			AddFunc("slice", IndexStartEndOperator.Instance);
-			AddFunc<List<object>, long, long, object[], List<object>>("splice", List_splice);
-			AddFunc<List<object>, long, List<object>>("splice", List_splice);
-			AddFunc<List<object>, long, long, List<object>>("splice", List_splice);
-			AddFunc(typeof(Enumerable), method =>
-			{
-				if (method.Name == "All") return "every";
-				if (method.Name == "Any") return "some";
-				if (method.Name == "FirstOrDefault") return "find";
-				return null;
-			});
 
 			AddTokenHandler("??", LazyTokenHandler.Instance);
 			AddTokenHandler("?=", LazyTokenHandler.Instance);
@@ -147,135 +118,6 @@ namespace AScript.Lang.JavaScript
 		public override ISyntaxAnalyzer GetSyntaxAnalyzer()
 		{
 			return JavaScriptSyntaxAnalyzer.Instance;
-		}
-
-		private static List<object> List_filter(List<object> list, Func<object, bool> func)
-		{
-			return list.Where(func).ToList();
-		}
-
-		private static List<object> List_map(List<object> list, Func<object, object> func)
-		{
-			return list.Select(func).ToList();
-		}
-
-		private static object List_reduce1(List<object> list, Func<object, object, object> func)
-		{
-			if (list.Count == 0) return null;
-			if (list.Count == 1) return list[0];
-			object r = list[0];
-			for (int i = 1; i < list.Count; i++)
-			{
-				r = func(r, list[i]);
-			}
-			return r;
-		}
-
-		private static object List_reduce2(List<object> list, Func<object, object, object> func, object init)
-		{
-			if (list.Count == 0) return init;
-			object r = init;
-			for (int i = 0; i < list.Count; i++)
-			{
-				r = func(r, list[i]);
-			}
-			return r;
-		}
-
-		private static long List_findIndex(List<object> list, Func<object, bool> func)
-		{
-			for (int i = 0; i < list.Count; i++)
-			{
-				if (func(list[i])) return i;
-			}
-			return -1L;
-		}
-
-		private static List<object> List_fill(List<object> list, object v)
-		{
-			for (int i = 0; i < list.Count; i++)
-			{
-				list[i] = v;
-			}
-			return list;
-		}
-
-		private static void List_forEach(List<object> list, Action<object> action)
-		{
-			list.ForEach(action);
-		}
-
-		private static object List_pop(List<object> list)
-		{
-			if (list == null || list.Count == 0) return null;
-			int index = list.Count - 1;
-			object v = list[index];
-			list.RemoveAt(index);
-			return v;
-		}
-
-		private static object List_shift(List<object> list)
-		{
-			if (list == null || list.Count == 0) return null;
-			object v = list[0];
-			list.RemoveAt(0);
-			return v;
-		}
-
-		private static long List_push(List<object> list, object v)
-		{
-			list.Add(v);
-			return list.Count;
-		}
-
-		private static long List_push(List<object> list, params object[] vs)
-		{
-			if (vs == null || vs.Length == 0) return list.Count;
-			if (vs.Length == 1) list.Add(vs[0]);
-			else list.AddRange(vs);
-			return list.Count;
-		}
-
-		private static long List_unshift(List<object> list, object v)
-		{
-			list.Insert(0, v);
-			return list.Count;
-		}
-
-		private static long List_unshift(List<object> list, params object[] vs)
-		{
-			if (vs == null || vs.Length == 0) return list.Count;
-			if (vs.Length == 1) list.Insert(0, vs[0]);
-			else list.InsertRange(0, vs);
-			return list.Count;
-		}
-
-		private static List<object> List_splice(List<object> list, long index)
-		{
-			int count = list.Count - (int)index;
-			return List_splice(list, index, count);
-		}
-
-		private static List<object> List_splice(List<object> list, long index, long count)
-		{
-			if (count == 0) return new List<object>();
-			var result = new List<object>((int)count);
-			for (int i = 0; i < count; i++)
-			{
-				result.Add(list[i + (int)index]);
-			}
-			list.RemoveRange((int)index, (int)count);
-			return result;
-		}
-
-		private static List<object> List_splice(List<object> list, long index, long count, params object[] addingList)
-		{
-			var result = List_splice(list, index, count);
-			if (addingList != null && addingList.Length > 0)
-			{
-				list.InsertRange((int)index, addingList);
-			}
-			return result;
 		}
 	}
 }
