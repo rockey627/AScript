fe066054d04b5b6502e9f5b7672ed8fac4cdb91c
rockey627 <584532801@qq.com>
2026-07-16 15:38:11 +0800

    1.4.1

--- AScript/IScriptModule.cs
+++ AScript/IScriptModule.cs
@@ -7,12 +7,4 @@ namespace AScript
 		object Install(BaseContext context);
 		void Uninstall(BaseContext context);
 	}
-
-	//public interface IScriptModuleType
-	//{
-	//	/// <summary>
-	//	/// 模块类型，Install返回的模块实例类型，主要用于编译模式下获取模块类型
-	//	/// </summary>
-	//	Type ModuleType { get; }
-	//}
 }
