36083102071ea6851aef7d14c28545c4df63df67
rockey627 <584532801@qq.com>
2026-08-25 17:22:59 +0800

    DynamicVariable动态变量

---
+++ AScript/DynamicVariable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AScript
+{
+	public class DynamicVariable
+	{
+		private readonly ScriptContext _context;
+		private readonly string _name;
+		private readonly bool _searchParent;
+
+		public DynamicVariable(ScriptContext context, string name, bool searchParent)
+		{
+			_context = context;
+			_name = name;
+			_searchParent = searchParent;
+		}
+
+		public object GetValue()
+		{
+			return GetValue(out _);
+		}
+
+		public object GetValue(out Type type)
+		{
+			return _context.EvalVar(_name, out type, _searchParent);
+		}
+	}
+}
