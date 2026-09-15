cdee830b2918217fecd808b565a307f4d0c7292f
rockey627 <584532801@qq.com>
2026-09-15 20:18:26 +0800

    IgnoreTokenHandler

---
+++ AScript/TokenHandlers/IgnoreTokenHandler.cs
@@ -0,0 +1,15 @@
+﻿using AScript.Syntaxs;
+using System;
+
+namespace AScript.TokenHandlers
+{
+	public class IgnoreTokenHandler : ITokenHandler
+	{
+		public static readonly IgnoreTokenHandler Instance = new IgnoreTokenHandler();
+
+		public void Build(DefaultSyntaxAnalyzer analyzer, TokenAnalyzingArgs e)
+		{
+			e.IsHandled = true;
+		}
+	}
+}
