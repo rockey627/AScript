ea77eab594a3f9e92de047b63986f35b03707636
rockey627 <584532801@qq.com>
2026-09-20 23:06:05 +0800

    Go：strconv

---
+++ AScript.Lang.Go/Extensions/strconv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AScript.Lang.Go.Extensions
+{
+	public class strconv
+	{
+#if NET45
+		public static Tuple<int, string> Atoi(string s)
+		{
+			if (int.TryParse(s, out var v))
+			{
+				return Tuple.Create<int, string>(v, null);
+			}
+			return Tuple.Create(0, $"strconv.Atoi: parsing \"{s}\": invalid syntax");
+		}
+#else
+		public static (int, string) Atoi(string s)
+		{
+			if (int.TryParse(s, out var v))
+			{
+				return (v, null);
+			}
+			return (0, $"strconv.Atoi: parsing \"{s}\": invalid syntax");
+		}
+#endif
+	}
+}
