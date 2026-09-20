80faa6d4855e0848640f2e6a05c0df1404a4bacd
rockey627 <584532801@qq.com>
2026-09-20 23:05:47 +0800

    GoConvertExtensions

---
+++ AScript.Lang.Go/Extensions/GoConvertExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AScript.Lang.Go.Extensions
+{
+	/// <summary>
+	/// 类型转换
+	/// </summary>
+	public static class GoConvertExtensions
+	{
+		public static double float64(object v)
+		{
+			return Convert.ToDouble(v);
+		}
+	}
+}
