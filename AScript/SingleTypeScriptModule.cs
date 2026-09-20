0e7dcfd6ca9a1b5ef17b1001c70e9170ac8803f9
rockey627 <584532801@qq.com>
2026-09-20 23:05:02 +0800

    SingleTypeScriptModule

---
+++ AScript/SingleTypeScriptModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AScript
+{
+	public class SingleTypeScriptModule : IScriptModule
+	{
+		public Type Type { get; private set; }
+		public string TypeName { get; private set; }
+
+		public SingleTypeScriptModule(Type type) : this(type, type.Name)
+		{
+		}
+		public SingleTypeScriptModule(Type type, string typeName)
+		{
+			this.Type = type;
+			this.TypeName = typeName;
+		}
+
+		public object Install(BaseContext context)
+		{
+			context.AddType(this.TypeName, this.Type);
+			return null;
+		}
+
+		public void Uninstall(BaseContext context)
+		{
+			context.RemoveType(this.TypeName);
+		}
+	}
+
+	public class SingleTypeScriptModule<T> : SingleTypeScriptModule
+	{
+		public SingleTypeScriptModule() : base(typeof(T)) { }
+		public SingleTypeScriptModule(string typeName) : base(typeof(T), typeName) { }
+	}
+}
