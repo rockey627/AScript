341f31baa99bdeefbd85a9b2aa55be73f35d371f
rockey627 <584532801@qq.com>
2026-09-21 14:51:55 +0800

    SingleTypeScriptModule.ReturnMode

--- AScript/SingleTypeScriptModule.cs
+++ AScript/SingleTypeScriptModule.cs
@@ -7,10 +7,19 @@ namespace AScript
 		public Type Type { get; private set; }
 		public string TypeName { get; private set; }
 
-		public SingleTypeScriptModule(Type type) : this(type, type.Name)
+		/// <summary>
+		/// <para>Install方法返回值：</para>
+		/// <para>0-返回null</para>
+		/// <para>1-返回Type</para>
+		/// <para>2-返回TypeWrapper</para>
+		/// <para>3-返回实例</para>
+		/// </summary>
+		public int ReturnMode { get; set; }
+
+		public SingleTypeScriptModule(Type type) : this(type.Name, type)
 		{
 		}
-		public SingleTypeScriptModule(Type type, string typeName)
+		public SingleTypeScriptModule(string typeName, Type type)
 		{
 			this.Type = type;
 			this.TypeName = typeName;
@@ -19,7 +28,14 @@ namespace AScript
 		public object Install(BaseContext context)
 		{
 			context.AddType(this.TypeName, this.Type);
-			return null;
+			switch (this.ReturnMode)
+			{
+				case 1: return this.Type;
+				case 2: return new TypeWrapper(this.TypeName, this.Type);
+				case 3: return Activator.CreateInstance(this.Type);
+				default:
+					return null;
+			}
 		}
 
 		public void Uninstall(BaseContext context)
@@ -31,6 +47,35 @@ namespace AScript
 	public class SingleTypeScriptModule<T> : SingleTypeScriptModule
 	{
 		public SingleTypeScriptModule() : base(typeof(T)) { }
-		public SingleTypeScriptModule(string typeName) : base(typeof(T), typeName) { }
+		public SingleTypeScriptModule(string typeName) : base(typeName, typeof(T)) { }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="returnMode">
+		/// <para>Install方法返回值：</para>
+		/// <para>0-返回null</para>
+		/// <para>1-返回Type</para>
+		/// <para>2-返回TypeWrapper</para>
+		/// <para>3-返回实例</para>
+		/// </param>
+		public SingleTypeScriptModule(int returnMode) : base(typeof(T))
+		{
+			this.ReturnMode = returnMode;
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="typeName"></param>
+		/// <param name="returnMode">
+		/// <para>Install方法返回值：</para>
+		/// <para>0-返回null</para>
+		/// <para>1-返回Type</para>
+		/// <para>2-返回TypeWrapper</para>
+		/// <para>3-返回实例</para>
+		/// </param>
+		public SingleTypeScriptModule(string typeName, int returnMode) : base(typeName, typeof(T))
+		{
+			this.ReturnMode = returnMode;
+		}
 	}
 }
