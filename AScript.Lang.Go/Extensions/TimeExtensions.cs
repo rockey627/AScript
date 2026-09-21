83b1635ee678297054b4081ca8c0a6b25caa60b6
rockey627 <584532801@qq.com>
2026-09-21 21:24:29 +0800

    Go：time.Sleep

--- AScript.Lang.Go/Extensions/TimeExtensions.cs
+++ AScript.Lang.Go/Extensions/TimeExtensions.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Threading;
 
 namespace AScript.Lang.Go.Extensions
 {
 	public static class TimeExtensions
 	{
 		public static DateTime time_Now() => DateTime.Now;
+
+		public static void time_Sleep(int ms)
+		{
+			Thread.Sleep(ms);
+		}
 	}
 }
